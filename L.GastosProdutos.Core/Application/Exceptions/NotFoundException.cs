9fb0ee9b7bf6c3481d9e7cfc6901b5190a6dff5e
Leonardo Oliveira <poliveira.leonardo@gmail.com>
2024-05-28 19:42:19 -0300

    Feat: Adding custom Exception class.

---
+++ L.GastosProdutos.Core/Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,17 @@
+﻿namespace L.GastosProdutos.Core.Application.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException() : base("Entity not found")
+        {
+        }
+
+        public NotFoundException(string message) : base(message)
+        {
+        }
+
+        public NotFoundException(string message, Exception innerExeception) : base(message, innerExeception)
+        {
+        }
+    }
+}
