06fea0f9308c9b8b192e6c21cd24d37c5018517c
Leonardo Oliveira <poliveira.leonardo@gmail.com>
2024-05-28 19:14:36 -0300

    Feat: Adding implementations.

---
+++ L.GastosProdutos.Core/Application/Implementations/PackingRepository.cs
@@ -0,0 +1,33 @@
+﻿using L.GastosProdutos.Core.Application.Interfaces;
+using L.GastosProdutos.Core.Domain.Entities.Packing;
+
+namespace L.GastosProdutos.Core.Application.Implementations
+{
+    public class PackingRepository : IPackingRepository
+    {
+        public Task<IReadOnlyList<PackingEntity>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PackingEntity> GetByIdAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CreateAsync(PackingEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(string id, PackingEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ L.GastosProdutos.Core/Application/Implementations/RecipeRepository.cs
@@ -0,0 +1,33 @@
+﻿using L.GastosProdutos.Core.Application.Interfaces;
+using L.GastosProdutos.Core.Domain.Entities.Receipt;
+
+namespace L.GastosProdutos.Core.Application.Implementations
+{
+    public class RecipeRepository : IRecipeRepository
+    {
+        public Task<IReadOnlyList<RecipeEntity>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<RecipeEntity> GetByIdAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CreateAsync(RecipeEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(string id, RecipeEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
